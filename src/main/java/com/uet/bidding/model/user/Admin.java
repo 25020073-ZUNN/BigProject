b46de5f567301ca4620cd61fbf210ee6e0c9231d
hoang dung <dunghoangvolao123@gmail.com>
2026-04-02 10:48:28 +0700

    update Admin.java

--- src/main/java/com/uet/bidding/model/user/Admin.java
+++ src/main/java/com/uet/bidding/model/user/Admin.java
@@ -1,4 +1,33 @@
 package com.uet.bidding.model.user;
 
-public class Admin {
+public class Admin extends User{
+
+    //Attributes
+    private int adminLevel;
+
+
+    //Constructor
+    protected Admin(String username,String email,String passwordHash,int adminLevel){
+        super(username,email,passwordHash);
+        if (adminLevel<1||adminLevel>3) throw new IllegalArgumentException("PasswordLevel must be from 1 to 3");
+        this.adminLevel=adminLevel;
+    }
+
+    //Getter
+    @Override
+    public String getRole(){
+        return "ADMIN";
+    }
+    public int getAdminLevel(){
+        return this.adminLevel;
+    }
+    public boolean canDeleteAdmin(){
+        return adminLevel==3;
+    }
+    @Override
+    public void printInfo() {
+        super.printInfo();
+        System.out.println("Admin Lvl: " + adminLevel);
+    }
+
 }
