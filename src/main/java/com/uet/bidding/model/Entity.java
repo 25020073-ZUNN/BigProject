57364749cb8a00c11c12d9479a76ea904e7a2661
hoang dung <dunghoangvolao123@gmail.com>
2026-04-02 10:35:34 +0700

    update user and entity

--- src/main/java/com/uet/bidding/model/Entity.java
+++ src/main/java/com/uet/bidding/model/Entity.java
@@ -1,33 +1,38 @@
 package com.uet.bidding.model;
-import java.io.Serializable;
-import java.time.LocalDateTime;
 import java.util.UUID;
-// Lớp cơ sở cho hệ thống
-public abstract class Entity  implements Serializable{
-    // Encapsulation: Sử dụng protected/private kết hợp getter/setter
-    protected String id;
-    protected LocalDateTime createdAt;
-    public Entity() {
-        this.id = UUID.randomUUID().toString(); // id User được random khi tạo
-        this.createdAt = LocalDateTime.now();  // lưu thời gian tạo
+public  abstract class Entity{
+
+
+
+    //THuoc tinh
+    private final String id;
+    private final long createAt;
+
+
+
+
+    //CONSTRUCTOR
+    protected Entity(){
+        this.id=UUID.randomUUID().toString(); //ID NGAU NHIEN O DANG STRING
+        this.createAt=System.currentTimeMillis();  //THOI GIAN THUC;
     }
-    // Constructor có tham số cho trường hợp cần khôi phục dữ liệu từ Database.
-    public Entity(String id, LocalDateTime createdAt) {
-        this.id = id;
-        this.createdAt = createdAt;
-    }
-    // Getters và Setters đảm bảo tính đóng gói (Encapsulation)
+
+
+
+    //GETTER
     public String getId() {
         return id;
     }
-    public void setId(String id) {
-        this.id = id;
+    public long getCreateAt() {
+        return createAt;
     }
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
-    }
+
+
+
+    //METHOD
     public abstract void printInfo();
+    @Override
+    public String toString(){
+        return this.getId();
+    }
 }
