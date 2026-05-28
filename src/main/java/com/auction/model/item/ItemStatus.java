267d430743f3f82d33a406a28f34fa024c31a905
tranthuyet <tranthuyetcuongly2007@gmail.com>
2026-05-29 01:28:45 +0700

    take care of the rest

--- src/main/java/com/auction/model/item/ItemStatus.java
+++ src/main/java/com/auction/model/item/ItemStatus.java
@@ -2,6 +2,12 @@ package com.auction.model.item;
 
 /**
  * Enum biểu diễn trạng thái của phiên đấu giá
+ * Các trạng thái bao gồm:
+ * * - OPEN : Phiên đấu giá vừa được tạo
+ * * - RUNNING : Phiên đấu giá đang diễn ra
+ * * - FINISHED : Phiên đấu giá đã kết thúc *
+ * - PAID : Sản phẩm đã được thanh toán
+ * * - CANCELED : Phiên đấu giá đã bị hủy
  */
 public enum ItemStatus {
     OPEN,       // Mới tạo
